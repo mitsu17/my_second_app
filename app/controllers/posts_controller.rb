be92abcc67452221cf3ea5125709bc66536c245f
mitsu17 <66157250+mitsu17@users.noreply.github.com>
2021-02-10 00:10:51 +0900

    update

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,11 @@
 class PostsController < ApplicationController
 
   def index  # indexアクションを定義した
+<<<<<<< Updated upstream
     @post = "これはコントローラーで定義したインスタンス変数を確認するための文字列です"
+=======
+    @posts = Post.all
+>>>>>>> Stashed changes
   end
 
 end
